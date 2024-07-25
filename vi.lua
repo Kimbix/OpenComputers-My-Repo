f6c4c2563d03bc3777ba913703d9624702cb2f4e
Kimbix <alemanhumberto06@gmail.com>
2024-07-25 04:44:14 -0400

    Made vi.lua user cdn instead of raw

--- vi.lua
+++ vi.lua
@@ -1,6 +1,6 @@
 local shell = require("shell")
 
-local vi_url = "https://raw.githubusercontent.com/Kimbix/OpenComputers-My-Repo/main/vi.lua"
+local vi_url = "https://cdn.githubraw.com/Kimbix/OpenComputers-My-Repo/main/vi.lua"
 local vi_dest = "/bin/vi.lua"
 
 local args, options = shell.parse(...)
