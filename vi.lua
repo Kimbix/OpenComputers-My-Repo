3f672010d71af9247936772336d94feb616bb656
Kimbix <alemanhumberto06@gmail.com>
2024-07-25 04:25:00 -0400

    Updated vi.lua

--- vi.lua
+++ vi.lua
@@ -1,6 +1,10 @@
 local shell = require("shell")
 
+local vi_url = "https://raw.githubusercontent.com/Kimbix/OpenComputers-My-Repo/main/vi.lua"
+local vi_dest = "/bin/vi.lua"
+
 local args, options = shell.parse(...)
 
-print("ARGS:" .. args)
-print("OPTIONS:" .. options)
+if options.u then
+    shell.execute("wget -f" .. vi_url .. vi_dest)
+end
